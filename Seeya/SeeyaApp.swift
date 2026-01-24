3d430b95ac3fac93b8275b8dae50d42fd2b66ea8
jayner52 <ingram.jayne@gmail.com>
2026-01-24 11:31:46 -0500

    Initial Commit

---
+++ Seeya/SeeyaApp.swift
@@ -0,0 +1,17 @@
+//
+//  SeeyaApp.swift
+//  Seeya
+//
+//  Created by Jayne Ingram on 2026-01-24.
+//
+
+import SwiftUI
+
+@main
+struct SeeyaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
